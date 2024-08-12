bd29e1a6a38542be0c9ac1cddc54ad4b5889e342
danihello <danihello@gmail.com>
2024-08-12 22:00:54 +0300

    black_linting

--- orchestrator/resources/__init__.py
+++ orchestrator/resources/__init__.py
@@ -6,12 +6,12 @@ from dagster_airbyte import AirbyteResource
 from dagster_snowflake import SnowflakeResource
 from dagster import ConfigurableResource
 
-#configure airbyte resource
+# configure airbyte resource
 airbyte_resource = AirbyteResource(
     host=EnvVar("AIRBYTE_SERVER_NAME"),
     port="8000",
     username=EnvVar("AIRBYTE_USERNAME"),
-    password=EnvVar("AIRBYTE_PASSWORD")
+    password=EnvVar("AIRBYTE_PASSWORD"),
 )
 
 # configure snowflake resource
@@ -22,12 +22,15 @@ snowflake_resource = SnowflakeResource(
     database=EnvVar("SNOWFLAKE_DATABASE"),
     warehouse=EnvVar("SNOWFLAKE_WAREHOUSE"),
     role=EnvVar("SNOWFLAKE_ROLE"),
-    schema=EnvVar("SNOWFLAKE_SCHEMA")
+    schema=EnvVar("SNOWFLAKE_SCHEMA"),
 )
 
 # configure dbt project resource
-dbt_project_dir = Path(__file__).joinpath("..", "..","..", "dbt", "warehouse").resolve()
+dbt_project_dir = (
+    Path(__file__).joinpath("..", "..", "..", "dbt", "warehouse").resolve()
+)
 dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
+
 
 class PostgresDatabaseResource(ConfigurableResource):
     server_name: str
