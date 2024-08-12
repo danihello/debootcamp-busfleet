2e124d1aa361d6b8856628cfc0ace271c82d920b
danihello <danihello@gmail.com>
2024-08-12 23:58:52 +0300

    1

--- .github/bin/linter.sh
+++ .github/bin/linter.sh
@@ -8,7 +8,7 @@ main() {
     black "${TOP_DIR}"
 
     # Setup dbt
-    #dbt deps --project-dir "${TOP_DIR}"/dbt/warehouse
+    dbt deps --project-dir "${TOP_DIR}"/dbt/warehouse
 
     # Lint SQL
     sqlfluff fix -f "${TOP_DIR}"/dbt/warehouse --exclude-rules L009,L010,L011,L012,L013,L014,L015,L016,L017,L018,L019,L020,L021,L022,L023,L024,L025,L026,L027,L028,L029,L030,L031,L032,L033,L034,L035,L036,L037,L038,L039,L040,L041,L042,L043,L044,L045,L046,L047,L048,L049,L050,L051,L052,L053,L054,L055,L056,L057,L058 --dialect postgres
