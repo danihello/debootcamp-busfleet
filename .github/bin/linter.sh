d3fab81d3c54648c16ed70c9355a56395402b03e
danihello <danihello@gmail.com>
2024-08-12 23:36:14 +0300

    4

--- .github/bin/linter.sh
+++ .github/bin/linter.sh
@@ -11,7 +11,6 @@ main() {
     dbt deps --project-dir "${TOP_DIR}/dbt/warehouse"
 
     # Lint SQL
-    sqlfluff fix -f "${TOP_DIR}/dbt/warehouse/models/marts"
 
     # If the linter produce diffs, fail the linter
     if [ -z "$(git status --porcelain)" ]; then 
