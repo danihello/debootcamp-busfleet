c3d3bab3b9c8f326c78904813676a0b78a6728a7
danihello <danihello@gmail.com>
2024-08-12 22:55:32 +0300

    fixed_linting

--- dbt/warehouse/models/staging/trans.sql
+++ dbt/warehouse/models/staging/trans.sql
@@ -16,17 +16,17 @@ with raw_trans as (
         date,
         datedvehiclejourneyref as trip_id,
         progressrate as status,
-        to_timestamp(recordedattime) as recorded_datetime,
         distancetraveled::decimal(18, 1) as distance_traveled,
-        replace(vehicleref, operatorref || '_', '') as vehicle_id,
         latitude,
         longitude,
         directionref as direction_id,
         operatorref as agency_id,
-        replace(stoppointref, 'MTA_', '') as current_stop_id,
         publishedlinename as route_id,
         estimatedpassengercount,
         estimatedpassengercapacity,
+        to_timestamp(recordedattime) as recorded_datetime,
+        replace(vehicleref, operatorref || '_', '') as vehicle_id,
+        replace(stoppointref, 'MTA_', '') as current_stop_id,
         case when coalesce(lag(datedvehiclejourneyref) over (partition by vehicleref order by recordedattime), '') != datedvehiclejourneyref
                 and lead(datedvehiclejourneyref) over (partition by vehicleref order by recordedattime) != datedvehiclejourneyref
                 then 1
@@ -47,6 +47,7 @@ with raw_trans as (
 
 stg_trips as (
     select
+        *,
         vehicle_id || '-' || recorded_datetime as trans_id,
         case when lag(trip_id) over (partition by vehicle_id order by recorded_datetime) != trip_id then 1
             when lag(trip_id) over (partition by vehicle_id order by recorded_datetime) is null then 1
@@ -54,8 +55,7 @@ stg_trips as (
         end as trip_start,
         case when lead(trip_id) over (partition by vehicle_id order by recorded_datetime) != trip_id then 1
             else 0
-        end as trip_end,
-        *
+        end as trip_end
     from raw_trans
     where bad_trans = 0
         and rnk = 1
@@ -63,8 +63,8 @@ stg_trips as (
 
 trip_grp as (
     select
-        sum(trip_start) over (partition by vehicle_id order by recorded_datetime) as trip_grp,
-        *
+        *,
+        sum(trip_start) over (partition by vehicle_id order by recorded_datetime) as trip_grp
     from stg_trips
 )
 
@@ -84,8 +84,8 @@ select
     agency_id,
     current_stop_id,
     route_id,
-    avg(estimatedpassengercount) over (partition by trip_grp, vehicle_id) as trip_avg_psngr_count,
-    max(estimatedpassengercapacity) over (partition by trip_grp, vehicle_id) as trip_max_psngr_capacity,
     estimatedpassengercount,
-    estimatedpassengercapacity
+    estimatedpassengercapacity,
+    avg(estimatedpassengercount) over (partition by trip_grp, vehicle_id) as trip_avg_psngr_count,
+    max(estimatedpassengercapacity) over (partition by trip_grp, vehicle_id) as trip_max_psngr_capacity
 from trip_grp
